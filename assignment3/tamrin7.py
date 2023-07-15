170ffd0a1d28bf6e26143fba9abe6bf0f2ab2b6f
ArmanGeramiBW <137865421+ArmanGeramiBW@users.noreply.github.com>
2023-07-15 10:06:19 -0700

    Update tamrin7.py

--- assignment3/tamrin7.py
+++ assignment3/tamrin7.py
@@ -1,8 +1,6 @@
-
 Yekan = int(input('Please enter Yekan:'))
 Dahgan = int(input('Please enter dahgan:'))
 Sadgan = int(input('Please enter sadgan:'))
 Hezargan = int(input('Please enter hezargan:'))
-print(Yekan ,'Y')
-print(Dahgan,Yekan , 'DY')
-print(Sadgan,Dahgan,Yekan , 'SDY')
+Ashar = int(input('please enter ashar:'))
+print(Hezargan,Sadgan,Dahgan,Yekan,".",Ashar)
