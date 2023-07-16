e9155fcc77382671701eea89d2fdebf204c3ab83
ArmanGeramiBW <137865421+ArmanGeramiBW@users.noreply.github.com>
2023-07-15 23:26:32 -0700

    Add files via upload

---
+++ assignment8/ATM.py
@@ -0,0 +1,17 @@
+passcode = 5007
+police_pass = 7005
+password = int(input("please enter your passcode:"))
+list_pass = []
+S = 0
+while S < 3 :
+   password = int(input("please enter your passcode:"))
+   if password == passcode :
+      print("welcome to your account")
+      break
+   elif (password) == police_pass:
+      print("In another minute you will be arrested by the police.")
+      break
+   else:
+      print("The password is wrong")
+      S += 1
+      
