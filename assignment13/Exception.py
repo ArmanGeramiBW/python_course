ef0725cc00900e64e5f1f2815e8df9e5084e1191
ArmanGeramiBW <137865421+ArmanGeramiBW@users.noreply.github.com>
2023-07-25 01:02:22 -0700

    Add files via upload

---
+++ assignment13/Exception.py
@@ -0,0 +1,13 @@
+#What's Exception : Thay're somethings that can handel your code when your cade has a error!
+##Some Exception : [ImportError , IndexError , KeyError , KeyboardInterrupt , NameError , OSError , OverflowError ,  RuntimeError , StopIteration]
+###We will Check some of theme:
+#1:ImportError = We can see this when => Raised when an imported module does not exist.
+##1 : For, suppose you want to use the math module, but this module is not installed in the Python runtime environment. In 
+##1this case, when using the import math command, it gets the error ImportError: No module named "math". 
+#2 NameError:
+##2 Raised when a variable does not exist
+###2 : In Python, NameError occurs because the name of the desired variable, function or class does not exist in
+###2 the Python execution environment. For example, if you use a variable in your code that is not already defined, you will receive a NameError.
+print(42 + x*3)
+print("NameError: name 's' is not defined")
+#We've looked at two examples of exceptions here, and we'll cover more of them on this page
