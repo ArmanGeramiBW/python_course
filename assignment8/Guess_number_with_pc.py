7f178916c52e81c0bebb9d57a48c66f6001176b9
ArmanGeramiBW <137865421+ArmanGeramiBW@users.noreply.github.com>
2023-07-31 21:57:31 -0700

    Update Guess_number_with_pc.py

--- assignment8/Guess_number_with_pc.py
+++ assignment8/Guess_number_with_pc.py
@@ -1,19 +1,22 @@
 import random
-x =int(input("Write your number:"))
 
-Big_num= 100
-Small_num = 0
-bot = random.randint(Small_num,Big_num)
-while True:
-    if bot == x:
-        print(f"bot is {bot}"," WE'VE GOT A WINNER!")
-        break
-    elif x < bot:
-        print(f"bot is {bot}","Go DOWN!")
-        Big_num = x
-        bot = random.randint(Small_num,Big_num)
-    else:
-        print(f"bot is {bot}","Go UP!")
-        Small_num = x
-        bot = random.randint(Small_num,Big_num)
-        #Helped from KIA
+def guessing_game():
+    low = 0
+    high = 100
+    count = 0
+
+    while True:
+        guess = random.randint(low,high)
+        count += 1
+        answer = input(f"My guess is {guess}. Is it too high (h), too low (l), or correct (c)? ")
+        if answer == "h":
+            high = guess - 1 #It means to measure less
+        elif answer == "l":
+            low = guess + 1 #It means to measure more
+        elif answer == "c":
+            print(f"I guessed your number in {count} times that you playing with me!")
+            break
+
+    print("Thanks for playing!See you soon :)")
+
+guessing_game()
