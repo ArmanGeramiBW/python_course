9bb4a2c03b9f1e4f148ce46f2bb836365b77a5cd
ArmanGeramiBW <137865421+ArmanGeramiBW@users.noreply.github.com>
2023-08-09 14:32:45 -0700

    Update Dict_Methods.py

--- assignment9/Dict_Methods.py
+++ assignment9/Dict_Methods.py
@@ -1,39 +1,64 @@
-# copy 
-Book = {"The writer": "Demitry Glokhofsky", "Model of Book": "Zombies story" , "Birth Year": "2018"}
-x = Book.copy()
-print(x)
+Dictionary = {
+"Brand": "Skynet",
+"Model": "killer of humans T800",         
+"Year of construction": "2074"}     
 
-# clear 
-Book = {"The writer": "Demitry Glokhofsky", "Model of Book": "Zombies story" , "Birth Year": "2018"}
-Book.clear()
-print(Book)
+repetition = int(input("How many times do you want to use?:"))
 
-# update 
-Book = {"The writer": "Demitry Glokhofsky", "Model of Book": "Zombies story" , "Birth Year": "2018"}
-Book.update({"papers": "seven hundred"})
-print(Book)
+print("""
+1 = copy
+2 = clear
+3 = pop
+4 = update
+5 = keys
+6 = values
+7 = items
+8 = get
+""")
 
-# keys 
-Book = {"The writer": "Demitry Glokhofsky", "Model of Book": "Zombies story" , "Birth Year": "2018"}
-x = Book.keys()
-print(x)
+for i in range(0 , repetition):
 
-# values 
-Book = {"The writer": "Demitry Glokhofsky", "Model of Book": "Zombies story" , "Birth Year": "2018"}
-x = Book.values()
-print(x)
+    methode = input("Which method do you want to use?!:")
+   
 
-# items 
-Book = {"The writer": "Demitry Glokhofsky", "Model of Book": "Zombies story" , "Birth Year": "2018"}
-x = Book.items()
-print(x)
+    if methode == "1":
+        copy = Dictionary.copy()
+        print("Copied:" , copy)
 
-# get 
-Book = {"The writer": "Demitry Glokhofsky", "Model of Book": "Zombies story" , "Birth Year": "2018"}
-x = Book.get("Model of Book")
-print(x)
+    elif methode == "2":
+        Dictionary.clear()
+        print("dictionary:" , Dictionary)
+ 
+    elif methode == "3":
+        p = input("What kay do you want to remove?: ")
+        Dictionary.pop(p)
+        print("dictionary:" , Dictionary) 
 
-## pop
-Book = {"The writer": "Demitry Glokhofsky", "Model of Book": "Zombies story" , "Birth Year": "2018"}
-Book.pop("Model of Book")
-print(Book)
+    elif methode == "4":
+        a = input("Please type your key that you want to insert:") 
+        a2 = input("Please type your value that you want to insert:")
+        #From a to a2
+        Dictionary.update({a : a2})
+        print("dictionary:" , Dictionary)
+
+    elif methode == "5":
+        k = Dictionary.keys()
+        print("keys:" , k)
+
+    elif methode == "6":
+        v = Dictionary.values()
+        print("values:", v)
+
+    elif methode == "7":
+        i = Dictionary.items()
+        print("items:", i)
+
+    elif methode == "8":
+        u = input()
+        Dictionary.update(u)
+        print("dictionary:",Dictionary)
+
+    else:
+        print(f"method number should be 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 not {methode}.")
+
+print("Thank you for using us. See you soon😁😄😉")
