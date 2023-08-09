d7979b693d649e30048601bbd071ac9414148b8d
ArmanGeramiBW <137865421+ArmanGeramiBW@users.noreply.github.com>
2023-08-09 09:44:44 -0700

    Update Set_Metodes.py

--- assignment9/Set_Metodes.py
+++ assignment9/Set_Metodes.py
@@ -1,31 +1,57 @@
-#add = add something to our set
-SET1 = {"Blue , Black , White"}
-SET1.add("Pink")
-print(SET1)
+# Create a set
+my_set = {"Green", "Blue", "Black", "White", "Red"}
 
-#Remove = REmove something from our set
-SET2 = {"Blue" , "orange" , "White"}
-SET2.remove("Blue")
-print(SET2)
+# Get the number of times to repeat the loop
+number_of_repetition = int(input("How many times do you want to use?:"))
 
-#copy = Copy something from our set.
-SET3 = {"Blue", "Black", "White"}
-a = SET3.copy()
-print(a)
+# Loop through the set functions
+for i in range(0, number_of_repetition):
+    # Get the method to try
+    print("""
+1 = add
+2 = remove
+3 = copy
+4 = clear
+5 = discard
+6 = update
+""")
+    our_method = int(input("Which method do you want to try? (Enter a number from 1 to 6): "))
+    
+    # Print the set
+    print(my_set)
 
-#clear = Remove all things in a set
+    # Try the selected method
+    if our_method == 1:
+        input_str = input("Please write what you want to add to your set: ")
+        my_set.add(input_str)
+        print("Set:", my_set)
+    # Remove something from our set.
+    elif our_method == 2:
+        input_str = input("Please write what you want to remove from your set: ")
+        my_set.remove(input_str)
+        print("Set:", my_set)
+    # Copy something from our set.
+    elif our_method == 3:
+        new_set = my_set.copy()
+        print("New Set:", new_set)
+    # Remove all things in a set.
+    elif our_method == 4:
+        my_set.clear()
+        print("Set:", my_set)
+    # Remove something that we want to remove it.
+    elif our_method == 5:
+        input_str = input("Please write what you want to discard from your set: ")
+        my_set.discard(input_str)
+        print("Set:", my_set)
 
-SET4 = {"Blue", "Black", "White"}
-SET4.clear()
-print(SET4)
+    elif our_method == 6:
+        input_str = input("Please write what you want to insert into your set: ")
+        my_set.update(input_str)
+        print("Set:", my_set)
 
-#discard = Remove something that we want to remove it
-SET5= {"Blue", "Black", "White"}
-SET5.discard("Black")
-print(SET5)
+    else:
+        print(f"Input should be 1, 2, 3, 4, 5, or 6 not {our_method}")
 
-#update = 
-SET6 = {"Blue", "Black", "White"}
-tropical = {"Pink" , "Green" , "Gray"}
-SET6.update(tropical)
-print(SET6)
+# End of loop
+print("Thank you for using us. See you soon😁😄😉")
+
