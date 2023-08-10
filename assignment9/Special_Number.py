2c48c09203b6f89414674246991ba53a7a833f49
ArmanGeramiBW <137865421+ArmanGeramiBW@users.noreply.github.com>
2023-08-10 02:12:08 -0700

    Update Special_Number.py

--- assignment9/Special_Number.py
+++ assignment9/Special_Number.py
@@ -1,9 +1,20 @@
-num = int(input("please enter your number:"))
-answer = 0
-for i in range(1,num):
-    if num % i == 0:
-        answer += i
-if answer == num:
-    print("Yes,this is ok")
+# Initialize a variable c to store the sum of divisors
+c = 0
+# Get an integer input from the user and store it in number
+number = int(input("Enter Your Number:"))
+
+# Loop through all the numbers from 1 to number-1
+for i in range(1,number):
+    # Check if number is divisible by i
+    if number % i == 0:
+        # Add i to c
+        c += i
+    
+# ]f c is equal to number
+if c == number:
+    # Print a message that the number is perfect
+	print("Yes this is a perfect number.")
+    
 else:
-    print("No,this isn't ok")
+    # Print a message that the number is not perfect
+	print("No this isn't a perfect number.")
